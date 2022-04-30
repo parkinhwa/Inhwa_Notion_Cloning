6c0de1451a3f1ca875f9c3159d4551cc3c880a2e
parkinhwa <inhwa6548@naver.com>
2022-04-30 16:41:15 +0900

    refactor: api 수정

--- src/api/document.js
+++ src/api/document.js
@@ -2,25 +2,25 @@ import { request } from "./api.js";
 
 const getDocuments = async (id) => {
   const url = id ? `${id}` : "";
-  return await request(`/${url}`, { method: "GET" });
+  return await request(`/documents/${url}`, { method: "GET" });
 };
 
 const createDocument = async (document) => {
-  return await request("", {
+  return await request("/documents/", {
     method: "POST",
     body: JSON.stringify(document),
   });
 };
 
 const updateDocument = async (id, document) => {
-  return await request(`/${id}`, {
+  return await request(`/documents/${id}`, {
     method: "PUT",
     body: JSON.stringify(document),
   });
 };
 
 const deleteDocument = async (id) => {
-  return await request(`/${id}`, {
+  return await request(`/documents/${id}`, {
     method: "DELETE",
   });
 };
