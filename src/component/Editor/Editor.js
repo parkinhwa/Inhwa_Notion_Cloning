0a6ecd7fb8fdaa66d7215d340c3f8b70d30ad5c5
parkinhwa <inhwa6548@naver.com>
2022-04-30 16:42:42 +0900

    refactor: render에서 dom 요소 추가, 값 설정하도록 함

--- src/component/Editor/Editor.js
+++ src/component/Editor/Editor.js
@@ -15,26 +15,25 @@ export default function Editor({
 
   this.setState = (nextState) => {
     this.state = nextState;
-    $editor.querySelector("[name=title]").value = this.state.title;
-    $editor.querySelector("[name=content]").value = this.state.content;
-
     this.render();
   };
 
   this.render = () => {
     if (!isInitialize) {
       $editor.innerHTML = `
-      <input type="text" name="title" placeholder="제목 없음" style="width:800px;"/>
-      <textarea name="content" placeholder="내용을 입력해주세요" style="width:800px;height:400px; border: 1px solid black; padding: 8px;">${this.state.content}</textarea>
+      <input class="editor-title" type="text" name="title" placeholder="제목 없음"/>
+      <textarea class="editor-content" name="content" placeholder="내용을 입력해주세요">${this.state.content}</textarea>
   `;
       isInitialize = true;
     }
+
+    $editor.querySelector("[name=title]").value = this.state.title;
+    $editor.querySelector("[name=content]").value = this.state.content;
   };
 
   this.render();
   $editor.addEventListener("keyup", (e) => {
     const { target } = e;
-
     const name = target.getAttribute("name");
     if (this.state[name] !== undefined) {
       const nextState = {
